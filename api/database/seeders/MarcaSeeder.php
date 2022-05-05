515f16a5421e403c900031f1bf1d21dccc63463f
Matheus <matheuscantarutti@gmail.com>
2022-05-05 20:23:59 -0300

    config Marca seeder

---
+++ api/database/seeders/MarcaSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MarcaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $marcas = [
+            'Electrolux',
+            'Brastemp',
+            'Fischer',
+            'Samsung',
+            'LG',
+        ];
+
+        foreach ($marcas as $marca) {
+            DB::table('marcas')->insert([
+                'name' => $marca,
+            ]);
+        }
+
+
+    }
+}
