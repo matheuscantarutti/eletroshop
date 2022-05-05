4e3e330cda7fa85ceabdd852cb456e98d0e7d97d
Matheus <matheuscantarutti@gmail.com>
2022-05-05 20:14:10 -0300

    criação de marca factory

---
+++ api/database/factories/MarcaFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Marca>
+ */
+class MarcaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $faker = \Faker\Factory::create('pt_BR');
+        return [
+            'nome' => $faker->randomElement([
+                'Electrolux',
+                'Brastemp',
+                'Fischer',
+                'Samsung',
+                'LG',
+            ])
+        ];
+    }
+}
